2143f1d92eaf4bf6e7d014fb5ad8d16d98558ac0
g2k4yh6p1-admin <g2k4yh6p1@gmail.com>
2024-11-08 11:21:41 -0500

    GetSuitFromCardIndex ✅ GetValueFromCardIndex✅ GetScoreFromCardValue✅ GetHandScore✅ ShowScore✅ GetHighestCardValue❌

--- tp2-a2024-majeurbilly/TP2/Game.cs
+++ tp2-a2024-majeurbilly/TP2/Game.cs
@@ -5,6 +5,7 @@ namespace TP2
     public class Game
     {
         #region Constants
+
         public const int HEART = 0;
         public const int DIAMOND = 1;
         public const int SPADE = 2;
@@ -39,31 +40,44 @@ namespace TP2
         public const int SAME_COLOR_SEQUENCE_SCORE = 28;
         public const int SEQUENCE_SCORE = 26;
         public const int SAME_COLOR_SCORE = 24;
+
         #endregion
 
         public static int GetSuitFromCardIndex(int index)
         {
             return index / NUM_CARDS_PER_SUIT;
         }
+
         public static int GetValueFromCardIndex(int index)
         {
             return index % 13;
-            
         }
 
         public static void DrawFaces(int[] cardValues, bool[] selectedCards, bool[] availableCards)
         {
-            // PROF : À COMPLETER.
+            for (int i = 0; i < selectedCards.Length; i++)
+            {
+                if (selectedCards[i] == false)
+                    do
+                    {
+                        selectedCards[i] = availableCards[new Random().Next(0, 52)];
+                        
+                    } while (selectedCards[i] == false);
+            }
         }
+
         public static int GetScoreFromCardValue(int cardValue)
         {
-            // PROF : À COMPLETER. Le code ci-après est incorrect
-            return 0;
+            return cardValue % 13;
         }
 
         public static int GetHandScore(int[] cardIndexes)
         {
-            // PROF : À COMPLETER. Le code ci-après est incorrect
+            //todo: pas fini 
+            int handScore = 0;
+            int[] handCards = new int[cardIndexes.Length];
+            
+
             return 0;
         }
 
@@ -76,13 +90,51 @@ namespace TP2
         // de tests avec vos noms de fonctions).
 
 
-
-
-
         public static void ShowScore(int[] cardIndexes)
         {
             int hand = GetHandScore(cardIndexes);
             Display.WriteString($"Votre score est de : {hand}", 0, Display.CARD_HEIGHT + 14, ConsoleColor.Black);
+        }
+
+
+        public static int GetHighestCardValue(int[] values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool HasOnlySameColorCards(int[] suits)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool HasAllSameCardValues(int[] values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool HasAllFaces(int[] values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool HasOnlyFaces(int[] values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool HasSameColorSequence(int[] values, int[] suits)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool HasSequence(int[] values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int GetScoreFromMultipleCardsOfASuit(int suit, int[] values, int[] suits)
+        {
+            throw new NotImplementedException();
         }
     }
 }
